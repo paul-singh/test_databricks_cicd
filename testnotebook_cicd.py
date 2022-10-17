003877c3de08a57135e604a005fc2cde60cef979
paul-singh <paul-singh>
2022-10-17 20:47:11 +0000

    PSinghCommitedIt

--- testnotebook_cicd.py
+++ testnotebook_cicd.py
@@ -1,3 +1,14 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC Hello World!
+
+# COMMAND ----------
+
+#Databricks notebook source
+df = spark.read.format("csv") \
+    .option("inferSchema","true") \
+    .option("header", "true") \
+    .option("sep",",") \
+    .load("/FileStore/tables/ind.csv")
+
+display(df)
